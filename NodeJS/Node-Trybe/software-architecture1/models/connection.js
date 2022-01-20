e8f5b06c8a618b8daca2a93ccfd77b3f40c322e1
Lucas Duarte <lucs.rkt@gmail.com>
2022-01-20 17:21:36 -0300

    feat: creaating .env

--- NodeJS/Node-Trybe/software-architecture1/models/connection.js
+++ NodeJS/Node-Trybe/software-architecture1/models/connection.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  user: 'lucas',
-  password: 'lucas18273645',
-  host: 'localhost',
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  host: process.env.HOST,
   database: 'model_example',
 });
 
--- NodeJS/Node-Trybe/software-architecture2/index.js
+++ NodeJS/Node-Trybe/software-architecture2/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Author = require('./controllers/Author');
+require('dotenv').config();
 
 const app = express();
 const PORT = 3000;
